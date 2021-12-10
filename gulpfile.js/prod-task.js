a6f3738d3dfc08a083bcd24b90ccdb915716296a
wonsleved <kraev.2014@gmail.com>
2021-12-10 14:51:03 +0300

    CLEAR

--- gulpfile.js/prod-task.js
+++
@@ -1,8 +0,0 @@
-const compileLess   = require('./compile-less').prod;
-const copyHtml      = require('./copy-html').prod;
-const { series, task } = require('gulp');
-
-task('compile-less', compileLess);
-task('copy-html', copyHtml);
-
-module.exports = series('compile-less', 'copy-html');
