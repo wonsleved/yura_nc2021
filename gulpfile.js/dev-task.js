b9deed73f217d06389b23b42f01be37dcf8abf6c
wonsleved <kraev.2014@gmail.com>
2021-12-10 14:51:03 +0300

    gulp prettier auto

---
+++ gulpfile.js/dev-task.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const bSync = require('browser-sync').create();
+const prettier = require('gulp-prettier');
+const cache = require('gulp-cached');
+
+const copyHtml = require('./copy-html').dev;
+const compileLess = require('./compile-less').dev;
+const paths = require('./paths');
+const { dev } = require('./copy-html');
+
+gulp.task('copy-html', copyHtml);
+gulp.task('compile-less', compileLess);
+
+function makePrettier() {
+    return gulp
+    .src('./src/**', {base: './src/'})
+    .pipe(cache())  
+    .pipe(prettier())
+    .pipe(gulp.dest('./src'));
+}
+
+gulp.task('make-prettier', makePrettier);
+
+function devTask() {
+    bSync.init({
+        server: { 
+            baseDir: paths.baseDir
+        }
+    });
+
+    gulp.watch(paths.bsyncPaths.styles, gulp.series('make-prettier', 'compile-less') ).on('change', bSync.reload);
+    gulp.watch(paths.bsyncPaths.html, gulp.series('make-prettier', 'copy-html') ).on('change', bSync.reload);
+}
+
+module.exports = devTask;
