d22ce1393e3ff3c04e54fe7f147858f7c02752f5
wonsleved <kraev.2014@gmail.com>
2022-01-10 18:51:08 +0300

    Initial commit

---
+++ gulpfile.js/compile-less.js
@@ -0,0 +1,29 @@
+const paths = require('./paths');
+const gulp  = require('gulp');
+const less  = require('gulp-less');
+const bsync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+
+function compileLessDev() {
+  return gulp
+    .src( paths.styles.src )
+    .pipe( sourcemaps.init() )
+    .pipe( less() )
+    .pipe( sourcemaps.write() )
+    .pipe( gulp.dest(paths.styles.dest) )
+    .pipe( bsync.stream() );
+};
+
+function compileLessProd() {
+  return gulp
+    .src( paths.styles.src )
+    .pipe( less() )
+    .pipe( gulp.dest(paths.styles.dest) );
+};
+
+const compileLess = {
+  prod: compileLessDev,
+  dev: compileLessProd
+}
+
+module.exports = compileLess;
